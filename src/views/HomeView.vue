bd10b8e94e8a6e8e378ec1f04765413956324343
0m3ga13 <oussama13-crb@hotmail.fr>
2023-02-28 21:41:48 +0100

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,94 @@
+
+<template>
+  <section class="text-gray-600 dark:bg-gray-900 body-font h-full">
+    <div class="container px-5 py-10 mx-auto flex flex-col">
+      <div class="lg:w-4/6 mx-auto">
+        <div class="rounded-lg h-64 overflow-hidden">
+          <img alt="content" class="object-cover object-center h-full w-full"
+            src="https://source.unsplash.com/OQMZwNd3ThU/1600x900">
+
+        </div>
+        <div class="flex flex-col sm:flex-row mt-10">
+          <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+            <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+              <img class="rounded-full"
+                src="https://scontent.falg6-1.fna.fbcdn.net/v/t1.6435-1/142707278_932260030851353_4723067606716334235_n.jpg?stp=dst-jpg_s200x200&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeHv6zqiRU8EFXgUXl2vDGg6pFx1jExT3yukXHWMTFPfK2Y-0v8Yx_mMFsg26K0DNJ1O77mWbWw9-dKiegKvPInQ&_nc_ohc=0fsFHoKSmnkAX9y_sdv&_nc_ht=scontent.falg6-1.fna&oh=00_AfCZ9cESFLik30eSqh97PZPdT-x4uf-inG0iecryGA271A&oe=6425BA28"
+                alt="">
+            </div>
+            <div class="flex flex-col items-center text-center justify-center">
+              <h2 class="font-medium title-font mt-4 text-gray-900 dark:text-gray-400 text-lg">أسامة سيد</h2>
+              <div class="w-12 h-1 bg-purple-500 rounded mt-2 mb-4"></div>
+              <p class="text-base">+213 782 679 003</p>
+              <p class="text-base">عين طاية - الجـزائر</p>
+
+            </div>
+          </div>
+          <div class="grid grid-rows-2 sm:w-2/3 sm:pl-8  sm:border-l dark:border-gray-200  sm:border-t-0 border-t sm:mb-0 sm:mt-0 text-right ">
+      
+            <div class="text-md  grid grid-rows-5 pt-10 ">
+
+            <p class="rtl ">مدرب في التسويق و التجارة الدولية
+            </p>
+            <p class="rtl pt-1">باحث دكتوراه تخصص تجارة دولية. و خدمات لوجيستية.
+            </p>
+            <p class="rtl pt-1">
+              مستشار دولي في التجارة الدولية
+            </p>
+            <p class="rtl pt-1">
+              مؤلف إقتصادي
+            </p>
+            <p class="rtl pt-1">
+              صانع محتوى
+            </p>
+
+            </div>
+          
+            <span 
+              class="inline-flex relative sm:justify-center justify-center sm:mt-5 pt-10">
+              <a href="https://www.facebook.com/profile.php?id=100022020935791" rel="noopener noreferrer" target="_blank"
+                class="text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
+                  viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a href="https://twitter.com/Oussama_sid36" rel="noopener noreferrer" target="_blank"
+                class="ml-6 text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                  </path>
+                </svg>
+              </a>
+              <a href="hhttps://instagram.com/oussama_sid36" rel="noopener noreferrer" target="_blank"
+                class="ml-6 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  class="w-5 h-5" viewBox="0 0 24 24">
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a href="https://www.linkedin.com/in/oussama-sid-83415919a/" rel="noopener noreferrer" target="_blank" class="ml-6 text-gray-500">
+                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                  stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.rtl {
+  float: right;
+
+}
+</style>
